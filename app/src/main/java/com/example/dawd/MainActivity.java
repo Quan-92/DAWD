686c3ea98534c31d1355b600b63b1aa48041c801
Quan-92 <71068237+Quan-92@users.noreply.github.com>
2022-09-22 23:13:15 +0700

    đawd

--- app/src/main/java/com/example/dawd/MainActivity.java
+++ app/src/main/java/com/example/dawd/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.dawd;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -20,10 +21,28 @@ public class MainActivity extends AppCompatActivity {
     Button btnAdd, btnUpdate, btnDelete;
     private static Employee employeeSelected;
 
+    boolean isAllFieldsChecked = false;
+
+    private boolean CheckAllFields() {
+        if (edName.length() == 0) {
+            edName.setError("This field is required");
+            return false;
+        }
+        if (edDesignation.length() == 0) {
+            edDesignation.setError("This field is required");
+            return false;
+        } else if (edSalary.length() == 0) {
+            edSalary.setError("Need to enter number");
+            return false;
+        }
+        return true;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         listViewEmployee = findViewById(R.id.list_view);
         sqLiteDatabaseHandle = new SQLiteDatabaseHandle(this);
         employees = sqLiteDatabaseHandle.getAllEmployees();
@@ -39,7 +58,14 @@ public class MainActivity extends AppCompatActivity {
 
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
+                isAllFieldsChecked = CheckAllFields();
+
+                if (isAllFieldsChecked) {
+                    Intent i = new Intent(MainActivity.this, MainActivity.class);
+                    startActivity(i);
+                }
+
                 Employee employee = new Employee();
                 employee.setName(edName.getText().toString());
                 employee.setDesignation(edDesignation.getText().toString());
@@ -51,7 +77,14 @@ public class MainActivity extends AppCompatActivity {
 
         btnUpdate.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
+                isAllFieldsChecked = CheckAllFields();
+
+                if (isAllFieldsChecked) {
+                    Intent i = new Intent(MainActivity.this, MainActivity.class);
+                    startActivity(i);
+                }
+
                 employeeSelected.setName(edName.getText().toString());
                 employeeSelected.setDesignation(edDesignation.getText().toString());
                 employeeSelected.setSalary(Double.parseDouble(edSalary.getText().toString()));
@@ -60,9 +93,25 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isAllFieldsChecked = CheckAllFields();
+
+                if (isAllFieldsChecked) {
+                    Intent i = new Intent(MainActivity.this, MainActivity.class);
+                    startActivity(i);
+                }
+
+                sqLiteDatabaseHandle.deleteEmployee(employeeSelected);
+                employeeListAdapter.refreshData(sqLiteDatabaseHandle.getAllEmployees());
+            }
+        });
+
+
         listViewEmployee.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            public void onItemClick(AdapterView adapterView, View v, int i, long l) {
                 Employee employee = employees.get(i);
                 edName.setText(employee.getName());
                 edDesignation.setText(employee.getDesignation());
@@ -70,15 +119,9 @@ public class MainActivity extends AppCompatActivity {
                 employeeSelected = employee;
             }
         });
-
-        btnDelete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                sqLiteDatabaseHandle.deleteEmployee(employeeSelected);
-                employeeListAdapter.refreshData(sqLiteDatabaseHandle.getAllEmployees());
-            }
-        });
-
     }
+
+
+
 }
 
